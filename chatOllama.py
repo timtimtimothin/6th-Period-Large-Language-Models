e57010e553a3c49a335f3ea0ec0f7b694222e500
timtimtimothin <tnelms6688@gmail.com>
2025-12-10 18:31:23 +0000

    Changed some minor issues with Interpreter LLM

--- chatOllama.py
+++ chatOllama.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 Model = ChatOllama(model="Interpreter")
-Interpreter_Prompt = ChatPromptTemplate.from_template("You are an Artificial Intelligence solely used for interpretation. Read the provided {feasible} and decide if the prompt is feasible or not. If it is, print 'feasible'; if not print 'not feasible'.")
+Interpreter_Prompt = ChatPromptTemplate.from_template("You are an Artificial Intelligence solely used for interpretation. Read the provided {feasible} and decide if the prompt is feasible or not. If it is, say 'feasible'; if not satIn 'not feasible'.")
 Neanderthal_Prompt = ChatPromptTemplate.from_template("You are an Artificial Intelligence that is acting like a neanderthal. When responding to prompts, maintain a certain form outlined in your system message.")
 Parser = StrOutputParser()
 
@@ -12,9 +12,12 @@ Neanderthal_Chain = Neanderthal_Prompt | Model | Parser
 user_Text = input("What would you like to ask the neanderthal?")
 
 feasibility = Interpreter_Chain.invoke({"feasible":f"Decide if {user_Text} is feasible."})
-if feasibility == "feasible":
-    print("The data is feasible!")
-elif feasibility == "not feasible":
-    print("The data is not feasible.")
+if "not feasible" in feasibility:
+    print("The data is not feasible!")
+    print("Your data was identified not feasible because:")
+    print(feasibility)
+elif "feasible" in feasibility:
+    response = Neanderthal_Chain.invoke({})
+    print(response)
 else:
     print("An error has occurred.")
